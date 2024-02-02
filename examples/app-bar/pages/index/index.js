3183f9b467eeb579f1c0737c1b83315746952aae
binniexu <binniexu@tencent.com>
2024-02-02 17:06:22 +0800

    进入app-bar演示页面再展示

--- examples/app-bar/pages/index/index.js
+++ examples/app-bar/pages/index/index.js
@@ -23,16 +23,21 @@ Page({
     ]
   },
   onLoad() {
-    // if (typeof this.getAppBar === 'function' ) {
-    //   const appBarComp = this.getAppBar()
-    //   // appBarComp.setData({})
-    //   console.log('maxCoverSize', appBarComp.data.maxCoverSize)
-    //   this.setData({
-    //     maxCoverSize: appBarComp.data.maxCoverSize
-    //   })
-    // }
+    
     console.log('代码片段是一种迷你、可分享的小程序或小游戏项目，可用于分享小程序和小游戏的开发经验、展示组件和 API 的使用、复现开发问题和 Bug 等。可点击以下链接查看代码片段的详细文档：')
     console.log('https://developers.weixin.qq.com/miniprogram/dev/devtools/minicode.html')
+  },
+  onShow() {
+    if (typeof this.getAppBar === 'function' ) {
+      const appBarComp = this.getAppBar()
+      appBarComp.setData({
+        showAppbar: true
+      })
+      console.log('showAppbar', appBarComp.data.showAppbar)
+      // this.setData({
+      //   maxCoverSize: appBarComp.data.maxCoverSize
+      // })
+    }
   },
 
   goDetail(e) {
